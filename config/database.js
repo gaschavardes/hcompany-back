f84807cf6c13b2084d7869f925a6a08ffb7187f9
gaschavardes <chavardes.gaspard@gmail.com>
2024-11-08 17:59:05 +0100

    fix(test)

--- config/database.js
+++ config/database.js
@@ -44,7 +44,8 @@ module.exports = ({ env }) => {
     },
     sqlite: {
       connection: {
-        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+        // filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+		filename: env('DATABASE_FILENAME', path.join(__dirname, '..', '.tmp/data.db')),
       },
       useNullAsDefault: true,
     },
